59f564690daaa5cc9df21b37ede42412f906f5cf
Warehouseman <mhb.warehouseman@gmail.com>
2020-08-31 09:29:22 -0400

    Created 'Returnable' DocType
    Signed-off-by: Warehouseman <mhb.warehouseman@gmail.com>

---
+++ returnable/returnable/doctype/returnable/returnable.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Warehouseman and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Returnable', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ returnable/returnable/doctype/returnable/test_returnable.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Warehouseman and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestReturnable(unittest.TestCase):
+	pass
