20e11bdaa2bdbb80879f7201423c80ad0405b524
Martin H. J. Bramwell <martinhbranwell@gmail.com>
2020-09-23 18:30:10 -0400

    Enhance (batch): Adds preliminary client and server side validation
    Signed-off-by: Martin H. J. Bramwell <martinhbranwell@gmail.com>

--- returnable/returnable/doctype/returnable_batch/returnable_batch.js
+++ returnable/returnable/doctype/returnable_batch/returnable_batch.js
@@ -1,46 +1,192 @@
-// Copyright (c) 2020, Warehouseman and contributors
-// For license information, please see license.txt
+const returnablesBatch = cur_frm.fields_dict;
+const returnablesTable = returnablesBatch.bottles_moved.grid;
 
-function setMandatory(frm) {
+
+const toCust = '> Cust';
+const toStock = '> Stock';
+const fromCust = 'Cust >';
+const fromStock = 'Stock >';
+
+const custCust = `${fromCust}${toCust}`;
+const custStock = `${fromCust}${toStock}`;
+const stockCust = `${fromStock}${toCust}`;
+const stockStock = `${fromStock}${toStock}`;
+
+const full = { filters: [["Returnable", "state", "=", "Lleno"]] };
+const empty = { filters: [["Returnable", "state", "=", "Vacio"]] };
+const atClient = { filters: [["Returnable", "state", "=", "Donde Cliente"]] };
+;
+const notAtClient = { filters: [["Returnable", "state", "!=", "Donde Cliente"]] };
+
+const fromStockLookUp = {
+  'Envases IB Llenos - LSSA': full,
+  'Envases IB Sucios - LSSA': empty,
+  '': notAtClient,
+};
+
+
+const fromCustLookUp = (source) => {
+  const fil = { filters: [["Returnable", "state", "=", "Donde Cliente"], ["Returnable", "last_customer", "=", source]] };
+  console.dir(fil);
+  return fil;
+};
+
+// const setFromCust = () => empty;
+const setStockCust = () => full;
+
+const setStockStock = (stock = '', customer = '') => fromStockLookUp[stock];
+// const setStockStock = (stock = '', customer = '') => () => {
+//   console.log('!!!!!!!!');
+//   return 'qwerty';
+// };
+
+// const setCustCust = (stock = '', customer = '') => fromCustLookUp(customer);
+const setFromCust = (stock = '', customer = '') => fromCustLookUp(customer);
+
+const stateLookUp = {};
+// stateLookUp['Cust >> Cust'] = setFromCust;
+// stateLookUp['Cust >> Stock'] = setFromCust;
+// stateLookUp['Stock >> Cust'] = setStockCust;
+// stateLookUp['Stock >> Stock'] = setStockStock;
+stateLookUp[`${custCust}`] = setFromCust;
+stateLookUp[`${custStock}`] = setFromCust;
+stateLookUp[`${stockCust}`] = setStockCust;
+stateLookUp[`${stockStock}`] = setStockStock;
+
+function adjustForDirection(frm) {
   const dir = frm.doc.direction;
-  frm.set_df_property('to_customer', 'reqd', dir.includes('--> Cust'))
-  frm.set_df_property('from_customer', 'reqd', dir.includes('Cust -->'))
-  frm.set_df_property('to_stock', 'reqd', dir.includes('--> Stock'))
-  frm.set_df_property('from_stock', 'reqd', dir.includes('Stock -->'))
+  const TS = frm.doc.to_stock || '';
+  const TC = frm.doc.to_customer || '';
+  const FS = frm.doc.from_stock || '';
+  const FC = frm.doc.from_customer || '';
 
-  frm.set_df_property('to_customer', 'hidden', dir.includes('--> Stock'))
-  frm.set_df_property('from_customer', 'hidden', dir.includes('Stock -->'))
-  frm.set_df_property('to_stock', 'hidden', dir.includes('--> Cust'))
-  frm.set_df_property('from_stock', 'hidden', dir.includes('Cust -->'))
+  console.log(`Function adjustForDirection (${dir}) 
+    FS: >${FS}< 
+    FC: >${FC}< 
+    TS: >${TS}< 
+    TC: >${TC}<
+  `);
+
+  const fil = stateLookUp[dir](FS, FC);
+  console.log('fil...');
+  console.dir(fil);
+  returnablesTable.get_field('bottle').get_query = () => fil;
+
+  cur_frm.fields_dict.to_stock.get_query = 
+    () => ({ filters: [
+      ["Warehouse", "parent", "=", "Envases Iridium Blue - LSSA"],
+      ["Warehouse", "name", "!=", FS],
+    ] });
+
+  cur_frm.fields_dict.from_stock.get_query = 
+    () => ({ filters: [
+      ["Warehouse", "parent", "=", "Envases Iridium Blue - LSSA"],
+      ["Warehouse", "name", "!=", TS],
+    ] });
+
+
+  cur_frm.fields_dict.to_customer.get_query = 
+    () => ({ filters: [
+      ["Customer", "name", "!=", FC],
+    ] });
+
+  cur_frm.fields_dict.from_customer.get_query = 
+    () => ({ filters: [
+      ["Customer", "name", "!=", TC],
+    ] });
+
+  // cur_frm.get_field('to_stock').get_query = () => { filters: [["Warehouse", "parent", "=", "Envases Iridium Blue - LSSA"]] };
+  // cur_frm.get_field('from_stock').get_query = () => { filters: [["Warehouse", "parent", "=", "Envases Iridium Blue - LSSA"]] };
+
+  // returnablesTable.refresh_field('bottle');
+  frm.refresh_field('bottles_moved');
+
+  frm.set_df_property('to_customer', 'reqd', dir.includes(toCust));
+  frm.set_df_property('from_customer', 'reqd', dir.includes(fromCust));
+  frm.set_df_property('to_stock', 'reqd', dir.includes(toStock));
+  frm.set_df_property('from_stock', 'reqd', dir.includes(fromStock));
+
+  frm.set_df_property('to_customer', 'hidden', dir.includes(toStock));
+  frm.set_df_property('from_customer', 'hidden', dir.includes(fromStock));
+  frm.set_df_property('to_stock', 'hidden', dir.includes(toCust));
+  frm.set_df_property('from_stock', 'hidden', dir.includes(fromCust));
+
 }
 
 frappe.ui.form.on('Returnable Batch', {
 	setup: function(frm) {
-    console.log(`setup ${frm.doc.direction}`);
-    setMandatory(frm);
+    console.log(`setup ${frm.doc.direction} >${returnablesTable}<`);
+    console.dir(returnablesTable);
+    adjustForDirection(frm);
     frm.set_value('timestamp', frappe.datetime.now_datetime());
     frm.set_df_property('timestamp', 'hidden', 0);
     // frm.set_value('timestamp', Date.now());
-
   },
   onload: function(frm) {
     console.log('onload');
-    setMandatory(frm);
+    adjustForDirection(frm);
   },
-  refresh: function(frm) {
-    console.log('refresh');
+  refresh: async function(frm) {
+    console.log(`refreshed : ${frm.doc.direction}`);
+    // const resp = await cur_frm.call({
+    //   method: "loadReturnablesStates",
+    //   args: { direction: frm.doc.direction }
+    // });
+    // console.log(resp.message);
   },
   validate: (frm, cdt, cdn) => {
-    console.log(`Validating child ${cdn} (${cdt}):`);
+    console.log(`Validating '${cdt}' :: ${cdn}`);
   },
   direction: (frm, cdt, cdn) => {
-    console.log(`direction ${cdn} (${cdt}):`);
-    setMandatory(frm);
+    console.log(`Direction :: ${frm.doc.direction}`);
+    adjustForDirection(frm);
+    
+    frm.refresh();
   },
+  from_customer: (frm, cdt, cdn) => {
+    console.log(`From Customer '${frm.doc.from_customer}' to  TS: '${frm.doc.to_stock}' TC: '${frm.doc.to_customer}'`);
+    adjustForDirection(frm);
+    frm.refresh();
+  },
+  from_stock: (frm, cdt, cdn) => {
+    console.log(`From Stock '${frm.doc.from_stock}' to  TS: '${frm.doc.to_stock}' TC: '${frm.doc.to_customer}'`);
+    adjustForDirection(frm);    
+    frm.refresh();
+  },
+  to_customer: (frm, cdt, cdn) => {
+    console.log(`To Customer '${frm.doc.to_customer}' to  FS: '${frm.doc.from_stock}' FC: '${frm.doc.from_customer}'`);
+    adjustForDirection(frm);
+    frm.refresh();
+  },
+  to_stock: (frm, cdt, cdn) => {
+    console.log(`To Stock '${frm.doc.to_stock}' to  FS: '${frm.doc.from_stock}' FC: '${frm.doc.from_customer}'`);
+    adjustForDirection(frm);    
+    frm.refresh();
+  },
+  // to_stock: (frm, cdt, cdn) => {
+  //   // const fldFromStock = returnablesBatch.get_field('from_stock');
+  //   console.log(`To Stock '${frm.doc.to_stock}' from '${frm.doc.from_stock}'`);
+  //   // console.dir(fldFromStock);
+  //   adjustForDirection(frm);
+  //   // if (frm.doc.direction = stockStock) {
+  //   //   returnablesBatch.get_field('from_stock').get_query = 
+  //   //     () => ({ filters: [["Warehouse", "name", "=", "Lleno"]] });
+  //   // }
+  // },
   specify_date: (frm, cdt, cdn) => {
     console.log(`specify_date ${cdn} (${cdt}):  ${frm.doc.specify_date}` );
     console.dir(frappe.datetime);
     frm.set_df_property('timestamp', 'read_only', ! frm.doc.specify_date)
     frm.set_value('timestamp', frappe.datetime.now_datetime());
   }
+});
+
+frappe.ui.form.on('Returnable Batch Item', {
+  bottle: (frm, cdt, cdn) => {
+    console.log(`Returnable Batch Item:`);
+    console.log(`    | bottle ${cdn} (${cdt}):`);
+  },
+  form_render: (frm, cdt, cdn) => {
+    console.log(`Returnable Batch Item: form_render`);
+  },
 });
--- returnable/returnable/doctype/returnable_batch/returnable_batch.py
+++ returnable/returnable/doctype/returnable_batch/returnable_batch.py
@@ -3,8 +3,205 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _, msgprint, throw
 from frappe.model.document import Document
 
+TOCUST = '> Cust'
+TOSTOCK = '> Stock'
+FROMCUST = 'Cust >'
+FROMSTOCK = 'Stock >'
+
+CUSTCUST = FROMCUST + TOCUST
+CUSTSTOCK = FROMCUST + TOSTOCK
+STOCKCUST = FROMSTOCK + TOCUST
+STOCKSTOCK = FROMSTOCK + TOSTOCK
+
+qryGetState = "SELECT code FROM `tabReturnable` where state = '{0}' and code in ({1})";
+
+FULL = 'Lleno'
+INDETERMINATE = 'Confuso'
+DIRTY = 'Sucio'
+AT_CUSTOMER = 'Donde Cliente'
+
+stateLookup = {
+  'Envases IB Llenos - LSSA': FULL,
+  'Envases IB Rotos - LSSA': INDETERMINATE,
+  'Envases IB Sucios - LSSA': DIRTY
+}
+
+# lst = [['a', ''], ['b', ''], ['c', ''], ['d', '']]
+# csvString
+def list2String(lst):
+  sep = ''
+  csvString = ''
+  for val in lst:
+    csvString += sep + '"' + val + '"'
+    sep = ','
+  return csvString
+
+def listReturnablesMoved(moved):
+  rets = []
+  for returnable in moved:
+      rets.append(returnable.bottle)
+  return rets
+
+def custCust(frm):
+  print('custCust :: ' + frm.direction)
+  tc = frm.to_customer;
+  fc = frm.from_customer;
+
+  if tc == fc:
+    exceptionMessages.append('No hay como mover envases entre "{0}" y "{1}" '.format(tc, fc))
+
+  if not fc:
+    exceptionMessages.append('Debe especificar el cliente de origen.')
+
+  if not tc:
+    exceptionMessages.append('Debe especificar el cliente destinatario.')
+
+  toBeMoved = listReturnablesMoved(frm.bottles_moved)
+  # query = qryGetState.format(AT_CUSTOMER, list2String(toBeMoved))
+  # print(qryGetState.format(AT_CUSTOMER, list2String(toBeMoved)))
+
+  actualReturnablesAtCustomer = frappe.db.sql(qryGetState.format(AT_CUSTOMER, list2String(toBeMoved)))
+  lstUnavailables = [x for x in toBeMoved if x not in [r[0] for r in actualReturnablesAtCustomer]]
+  ul = len(lstUnavailables)
+  if 0 < len(lstUnavailables):
+    exceptionMessages.append(
+      noSuchProduct.format(fc, 'el' if ul == 1 else 'los', '' if ul == 1 else 's', list2String(lstUnavailables))
+    )
+  # raise Exception('No validation for: {}'.format(CUSTCUST))
+
+def custStock(frm):
+  print('custStock :: ' + frm.direction)
+  fc = frm.from_customer;
+  ts = frm.to_stock;
+
+  toBeMoved = listReturnablesMoved(frm.bottles_moved)
+  actualReturnablesAtCustomer = frappe.db.sql(qryGetState.format(AT_CUSTOMER, list2String(toBeMoved)))
+  lstUnavailables = [x for x in toBeMoved if x not in [r[0] for r in actualReturnablesAtCustomer]]
+
+  ul = len(lstUnavailables)
+
+  if 0 < len(lstUnavailables):
+    exceptionMessages.append(
+      noSuchProduct.format(fs, 'el' if ul == 1 else 'los', '' if ul == 1 else 's', list2String(lstUnavailables))
+    )
+
+  if not fc:
+    exceptionMessages.append('Debe especificar el cliente de origen.')
+
+  if not ts:
+    exceptionMessages.append('Debe especificar el almacen destinatario.')
+
+def stockCust(frm):
+  fs = frm.from_stock;
+  tc = frm.to_customer;
+  # print('stockCust :: ' + frm.direction)
+  # print(frm.from_stock)
+
+  toBeMoved = listReturnablesMoved(frm.bottles_moved)
+  # print(list2String(toBeMoved))
+
+  # query = qryGetState.format(FULL, list2String(toBeMoved))
+  # print(qryGetState.format(FULL, list2String(toBeMoved)))
+
+  actualReturnablesInWarehouse = frappe.db.sql(qryGetState.format(FULL, list2String(toBeMoved)))
+  # print("actualReturnablesInWarehouse {0} ".format(actualReturnablesInWarehouse))
+  # print(type(actualReturnablesInWarehouse))
+  # lstReturnables = [r[0] for r in actualReturnablesInWarehouse]
+  lstUnavailables = [x for x in toBeMoved if x not in [r[0] for r in actualReturnablesInWarehouse]]
+  # print(type([r[0] for r in actualReturnablesInWarehouse]))
+  # print(len(lstReturnables))
+  # print(type(lstUnavailables))
+  # print(len(lstUnavailables))
+
+  ul = len(lstUnavailables)
+
+  if 0 < len(lstUnavailables):
+    exceptionMessages.append(
+      noSuchProduct.format(fs, 'el' if ul == 1 else 'los', '' if ul == 1 else 's', list2String(lstUnavailables))
+    )
+
+  if not fs:
+    exceptionMessages.append('Debe especificar el almacen de origen.')
+
+  if not tc:
+    exceptionMessages.append('Debe especificar el cliente destinatario.')
+
+
+  # raise Exception('No validation for: {}'.format(STOCKCUST))
+
+def stockStock(frm):
+  print('stockStock :: ' + frm.direction)
+  fs = frm.from_stock;
+  ts = frm.to_stock;
+  state = stateLookup.get(fs, AT_CUSTOMER)
+  if state == AT_CUSTOMER:
+    exceptionMessages.append('No se puede mover envases desde "{0}" '.format(fs))
+
+  ts = frm.to_stock;
+  if stateLookup.get(ts, AT_CUSTOMER) == AT_CUSTOMER:
+    exceptionMessages.append('No se puede mover envases a "{0}" '.format(ts))
+
+  if ts == fs:
+    exceptionMessages.append('No hay como mover envases entre "{0}" y "{1}" '.format(ts, fs))
+
+  if not fs:
+    exceptionMessages.append('Debe especificar el almacen de origen.')
+
+  if not ts:
+    exceptionMessages.append('Debe especificar el almacen destinatario.')
+
+
+  toBeMoved = listReturnablesMoved(frm.bottles_moved)
+  actualReturnablesInWarehouse = frappe.db.sql(qryGetState.format(state, list2String(toBeMoved)))
+  lstUnavailables = [x for x in toBeMoved if x not in [r[0] for r in actualReturnablesInWarehouse]]
+  ul = len(lstUnavailables)
+  if 0 < len(lstUnavailables):
+    exceptionMessages.append(
+      noSuchProduct.format(fs, 'el' if ul == 1 else 'los', '' if ul == 1 else 's', list2String(lstUnavailables))
+    )
+
+
+directions = {}
+directions[CUSTCUST] = custCust
+directions[CUSTSTOCK] = custStock
+directions[STOCKCUST] = stockCust
+directions[STOCKSTOCK] = stockStock
+
+exceptionMessages = ['Errores:']
+# exceptionMessages = ['']
+noSuchProduct = 'Almacen "{0}" no tiene {1} producto{2} : {3}'
+
 class ReturnableBatch(Document):
-	pass
+	
+  def validate(self):
+
+    exceptionMessages.append('DUMMY')
+
+    print(' *** *** *** ***  Direction ', self.direction, ' <> ', ' *** *** *** *** *** ');
+    directions[self.direction](self)
+
+
+    if 1 < len(exceptionMessages):
+      exceptionMessage = ''
+      sep = ''
+      bullet = 1
+      for msg in exceptionMessages: 
+        exceptionMessage += sep + msg
+        sep = '\n ' + str(bullet) + ') '
+        bullet += 1
+      print(exceptionMessage)
+      exceptionMessages.clear()
+      exceptionMessages.append('Errores:')
+      frappe.throw(_(exceptionMessage))
+
+# @frappe.whitelist()
+# def loadReturnablesStates(direction):
+#   # msgprint(_("Direction is :: {0} ").format(direction) )
+#   actualReturnablesInWarehouse = frappe.db.sql("select count(*) from `tabReturnable`");
+
+#   print(' *** *** *** ***  Direction ', direction, ' <> ', type(actualReturnablesInWarehouse), ' *** *** *** *** *** ');
+#   return ("Returnables Count :: {0} ").format(actualReturnablesInWarehouse);
