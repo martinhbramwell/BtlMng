bb2b9533a1acb44f89cf9161d733679149421385
Martin H. J. Bramwell <martinhbranwell@gmail.com>
2020-09-24 08:41:09 -0400

    Enhance (customer): Adds 'Returnable Holder' child table to Customer
    Signed-off-by: Martin H. J. Bramwell <martinhbranwell@gmail.com>

---
+++ rSYNC.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )";
+export SCRIPT_NAME=$(basename "$0");
+
+export REMOTE_PROJECT_DIR="returnable";
+
+source ${HOME}/projects/FacElect/install_scripts/envars.sh;
+
+export REMOTE_PROJECT="${SERVER_ALIAS}:/home/${ERP_USER}/${TARGET_BENCH_NAME}/apps/${REMOTE_PROJECT_DIR}";
+
+echo -e "Synching this directory '$(pwd)' with :: ${REMOTE_PROJECT}";
+
+
+
+while inotifywait -qqr -e close_write,move,create,delete ./*; do
+  rsync -rzavx --update . ${REMOTE_PROJECT};
+done;
+
+  # echo -e "\n\n/* ~~~~~~~~~ Curtailed ~~~~~~~ ${SCRIPT_DIR}/${SCRIPT_NAME} ~~~~~~~~ */
+
+  # ";
+  # exit;
